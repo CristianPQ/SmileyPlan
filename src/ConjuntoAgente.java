5c0a297daa8ce8210cdd4722badf03ad0727b618
Daniel Villanueva <danielvillanueva94@gmail.com>
2014-10-29 18:30:07 +0100

    "ConjuntoAgentes" prenent forma

--- src/ConjuntoAgente.java
+++ src/ConjuntoAgente.java
@@ -1,22 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class ConjuntoAgente {
-		private int num_agentes = 0;
+		private int numAgentes = 0;
 		private Agente[] ConjAgentes;
 
-		public void añadirAgente(String id, String ciudad_ini, String ciudad_obj){ //ANADIR
+		public void añadirAgente(String id, String ciudad_ini, String ciudad_obj){ //ANADIR AGENTE
 			Agente a = new Agente();
 			a.setNombre(id);
 			a.setCiudad_inicial(ciudad_ini);
 			a.setCiudad_objetivo(ciudad_obj);
 			//ConjAgentes.add(a);
-			ConjAgentes[num_agentes] = a;
-			++num_agentes;
+			ConjAgentes[numAgentes] = a;
+			++numAgentes;
 		}	//FIN ANADIR
 		
-		public Agente consultarAgente(String nom){	//CONSULTAR
+		public int getNumAgentes(){ //consulta del numero de agentes que hay
+		return numAgentes;
+		}
+		
+		public Agente consultarAgente(String nom){	//CONSULTAR AGENTE
 			Agente a = new Agente();
 			boolean b = false;
-			for (int i = 0; i < num_agentes; ++i){
+			for (int i = 0; i < numAgentes; ++i){
 				
 				if (ConjAgentes[i].getNombre() == nom) {
 					a.setNombre(ConjAgentes[i].getNombre());
@@ -31,9 +37,55 @@ public class ConjuntoAgente {
 			return a;
 			} //FIN CONSULTAR
 			
+		public String consultar_ciudad_inicial(String nom){ //CONSULTAR CIUDAD INICIAL
+			boolean b = false;
+			String initial_city = "NoExisteAgenteConDichoNombre";
+			for (int i = 0; i < numAgentes; ++i){
+				
+				if (ConjAgentes[i].getNombre() == nom) {
+					initial_city = ConjAgentes[i].getCiudad_inicial();
+					b = true;					
+					}
+				
+				if (b) break;				
+				}
+			return initial_city;
+			} //FIN CONSULTAR INIT CITY
+			
+		public String consultar_ciudad_objetivo(String nom){ //CONSULTAR CIUDAD OBJ
+			boolean b = false;
+			String final_city = "NoExisteAgenteConDichoNombre";
+			for (int i = 0; i < numAgentes; ++i){
+				
+				if (ConjAgentes[i].getNombre() == nom) {
+					final_city = ConjAgentes[i].getCiudad_objetivo();
+					b = true;					
+					}
+				
+				if (b) break;				
+				}
+			return final_city;
+			}//FIN FINAL CITY
+		
+		public List<Agente> consultar_agentes_con_ciudad_inicial(String init_city){ //AG con CiIni concreta
+			List<Agente> l = new ArrayList<Agente>();
+			for (int i = 0; i < numAgentes; ++i)
+				if (ConjAgentes[i].getCiudad_inicial() == init_city) l.add(ConjAgentes[i]);
+			return l;
+			}
+		
+		public List<Agente> consultar_agentes_con_ciudad_objetivo(String final_city){ //AG con Cobj concreta
+			List<Agente> l = new ArrayList<Agente>();
+			for (int i = 0; i < numAgentes; ++i)
+				if (ConjAgentes[i].getCiudad_objetivo() == final_city) l.add(ConjAgentes[i]);
+			return l;
+			}
+		
 		}
-		//public String consultar_ciudad_inicialAgente a);
-		//public String consultar_ciudad_objetivo
+
+
+
+
 		//public list consultar_agentes_con_ciudad_inicial
 		//public list consultar_agentes_con_ciudad_objetivo
 
