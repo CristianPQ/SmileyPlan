7c5a4986e14bfdbdf2f77d51698218600dac5886
Cristian Pinto <quinto100@gmail.com>
2014-11-04 18:03:01 +0100

    ConjuntoCiudades y Ciudad

--- src/ConjuntoCiudades.java
+++ src/ConjuntoCiudades.java
@@ -12,32 +12,61 @@ public class ConjuntoCiudades {
 	 * Devuelve un Set con todas las keys de ciudades, en nuestro caso los nombres de las ciudades
 	 */
 	public Set<String> getCiudades() {
-		return ciudades.keySet();
+		return this.ciudades.keySet();
 	}
 	
-	//Anadir ciudad al Map ciudades
-	public void setCiudad(Ciudad c) {
+	/*
+	 * Anadir ciudad al Map ciudades
+	 */
+	public void agregarCiudad(Ciudad c) {
 		String nombreCiudad = c.getNombre();
-		ciudades.put(nombreCiudad, c);
+		this.ciudades.put(nombreCiudad, c);
 	}
 	
-	//Buscar Ciudad a partir del nombre
-	public Ciudad buscarCiudadPorNombre(String nombreCiudad) {
-		Ciudad c = ciudades.get(nombreCiudad);
+	/*
+	 * Buscar Ciudad a partir del nombre
+	 */
+	public Ciudad buscarCiudad(String nombreCiudad) {
+		Ciudad c = this.ciudades.get(nombreCiudad);
 		return c;
 	}
 	
-	//Consultar coordenadas de una ciudad
+	/*
+	 * Consultar coordenadas de una ciudad
+	 */
 	public Coordenadas getCoordenadasCiudad(String nombreCiudad) {
 		Coordenadas coord;
-		Ciudad c = buscarCiudadPorNombre(nombreCiudad);
+		Ciudad c = buscarCiudad(nombreCiudad);
 		coord = c.getCoord();
 		return coord;
 	}
 	
-	//Borrar ciudad
+	/*
+	 * Borrar ciudad
+	 */
 	public void borrarCiudad(String nombreCiudad) {
-		ciudades.remove(nombreCiudad);
+		this.ciudades.remove(nombreCiudad);
+	}
+	
+	/*
+	 * Consultar numero de ciudades existentes en el Map ciudades
+	 */
+	public int numeroCiudades() {
+		return this.ciudades.size();
+	}
+	
+	/*
+	 * Consultar caminos existentes desde una ciudad
+	 */
+	public ArrayList<Camino> conCiudadOrigen(String ciudadOrigen){
+		return this.caminos.getCiudadOrigen(ciudadOrigen);
+	}
+	
+	/*
+	 * Agregar camino en ConjuntoCaminos caminos
+	 */
+	public void agregarCamino(Camino c) {
+		this.caminos.agregarCamino(c);
 	}
 	
 
--- src/Mapa.java
+++ src/Mapa.java
@@ -3,23 +3,59 @@ import java.util.Set;
 
 public class Mapa {
 	
-	static private int anchuraX = 1000;
-	static private int alturaY = 1000;
+	private int anchuraX;
+	private int alturaY;
 	private ConjuntoCiudades ciudades;
 	private ConjuntoCaminos caminos;
 	private Coordenadas[] continente;
 	
-	public Mapa(Coordenadas[] continente) {
+	public Mapa(Coordenadas[] continente, int anchuraX, int alturaY) {
 		this.continente = continente;
+		this.anchuraX = anchuraX;
+		this.alturaY = alturaY;
 	}
 	
 	/*
 	 * Getter Set de nombres de ciudades
 	 */
-	public Set<String> todasLasCiudades() {
+	public Set<String> getCiudades() {
 		Set<String> ciudades = this.ciudades.getCiudades();
-		return ciudades;
-		
+		return ciudades;	
 	}
 	
+	/*
+	 * Agregar una nueva ciudad al ConjuntoCiudades ciudades
+	 */
+	public void agregarCiudad(Ciudad c) {
+		this.ciudades.agregarCiudad(c);
+	}
+	
+	/*
+	 *Consultar el numero de ciudades que tiene el ConjuntoCiudades 
+	 */
+	public int numeroCiudades() {
+		this.numeroCiudades();
+	}
+	
+	/*
+	 * Consultar el numero de ciudades que tiene el ConjuntoCaminos
+	 */
+	public int numeroCaminos() {
+		return this.caminos.getNumCaminos();
+	}
+	
+	//Buscar Ciudad a partir del nombre
+	public Ciudad buscarCiudad(String nombreCiudad) {
+		return this.ciudades.buscarCiudad(nombreCiudad);
+	}
+	
+	//Consultar coordenadas de una ciudad
+	public Coordenadas getCoordenadasCiudad(String nombreCiudad) {
+		return this.ciudades.getCoordenadasCiudad(nombreCiudad);
+	}
+	
+	//Borrar ciudad
+	public void borrarCiudad(String nombreCiudad) {
+		this.ciudades.borrarCiudad(nombreCiudad);
+	}
 }
