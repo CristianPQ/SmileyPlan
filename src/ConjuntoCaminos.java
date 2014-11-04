fca62bb9f6ebb1e7e5723a3d3678be1c1f614670
kiey <joan.marcual93@gmail.com>
2014-11-04 18:20:54 +0100

    conjutoCaminos

--- src/ConjuntoCaminos.java
+++ src/ConjuntoCaminos.java
@@ -5,46 +5,25 @@ public class ConjuntoCaminos {
 	private Map<String, ArrayList<Camino>> Caminos = new HashMap<String, ArrayList<Camino>>();
 	
 	
-	//crear un Camino con capacidad
-	public void agregarCaminoCapacidad(String ciudadOrigen, String ciudadDestino, int capacidad, String transporte) throws Exception{
-		ArrayList<Camino> listAux = new ArrayList<Camino>();  //creo una lista auxiliar para insertarle el Camino 
-		Camino auxCamino = new Camino(ciudadOrigen,ciudadDestino, capacidad, transporte); //Camino auxiliar
-		if (Caminos.containsKey(ciudadOrigen)) { //primer miro si hi ha algun cami amb ciutatorigen
-			if (Caminos.get(ciudadOrigen).contains(auxCamino)){ //miro si existe ese camino en concreto
-				throw new Exception("El Camino ya existe");   
-			}
-			else {
-				listAux = Caminos.get(ciudadOrigen); //mira si es pot fer sense treure la llista
-				listAux.add(auxCamino);
-				Caminos.put(ciudadOrigen, listAux);
-			}
-		}	
-		else { 
-			listAux = Caminos.get(ciudadOrigen);
-			listAux.add(auxCamino);
-			Caminos.put(ciudadOrigen, listAux);
-		}
-	}
-			
+	
 
 	//crear un Camino sin capcidad, 1 por defecto
-	public void agregarCamino(String ciudadOrigen, String ciudadDestino, String transporte) throws Exception{
+	public void agregarCamino(Camino camino) throws Exception{
 		ArrayList<Camino> listAux = new ArrayList<Camino>();  //creo una lista auxiliar para insertarle el Camino 
-		Camino auxCamino = new Camino(ciudadOrigen,ciudadDestino, transporte); //Camino auxiliar
-		if (Caminos.containsKey(ciudadOrigen)) { //primer miro si hi ha algun cami amb ciutatorigen
-			if (Caminos.get(ciudadOrigen).contains(auxCamino)){ //miro si existe ese camino en concreto
+		if (Caminos.containsKey(camino.getOrigen())) { //primer miro si hi ha algun cami amb ciutatorigen
+			if (Caminos.get(camino.getOrigen()).contains(camino)){ //miro si existe ese camino en concreto
 				throw new Exception("El Camino ya existe");   
 			}
 			else {
-				listAux = Caminos.get(ciudadOrigen); //mira si es pot fer sense treure la llista
-				listAux.add(auxCamino);
-				Caminos.put(ciudadOrigen, listAux);
+				listAux = Caminos.get(camino.getOrigen()); //mira si es pot fer sense treure la llista
+				listAux.add(camino);
+				Caminos.put(camino.getOrigen(), listAux);
 			}
 		}	
 		else { 
-			listAux = Caminos.get(ciudadOrigen);
-			listAux.add(auxCamino);
-			Caminos.put(ciudadOrigen, listAux);
+			listAux = Caminos.get(camino.getOrigen()); //mira si es pot fer sense treure la llista
+			listAux.add(camino);
+			Caminos.put(camino.getOrigen(), listAux);
 		}
 	}
 		
@@ -70,14 +49,13 @@ public class ConjuntoCaminos {
 			return Caminos.size();
 	}
 	
-	public void borrarCamino (String ciudadOrigen, String ciudadDestino, String transporte) throws Exception{
+	public void borrarCamino (Camino camino) throws Exception{
 		ArrayList<Camino> listAux = new ArrayList<Camino>();  //creo una lista auxiliar para borrar el Camino 
-		Camino auxCamino = new Camino(ciudadOrigen,ciudadDestino, transporte); //Camino auxiliar
-		if (Caminos.containsKey(ciudadOrigen)) { //primer miro si hi ha algun cami amb ciutatorigen
-			if (Caminos.get(ciudadOrigen).contains(auxCamino)){ //miro si existe ese camino en concreto
-				listAux = Caminos.get(ciudadOrigen); //mira si es pot fer sense treure la llista
-				listAux.remove(auxCamino); //borro
-				Caminos.put(ciudadOrigen, listAux); //inserto la nova llista axtualitzada
+		if (Caminos.containsKey(camino.getOrigen())) { //primer miro si hi ha algun cami amb ciutatorigen
+			if (Caminos.get(camino.getOrigen()).contains(camino)){ //miro si existe ese camino en concreto
+				listAux = Caminos.get(camino.getOrigen()); //mira si es pot fer sense treure la llista
+				listAux.remove(camino); //borro
+				Caminos.put(camino.getOrigen(), listAux); //inserto la nova llista axtualitzada
 			}
 			else {
 				throw new Exception("El Camino NO existe");   
@@ -88,29 +66,37 @@ public class ConjuntoCaminos {
 		}
 	}	
 	//devuelve todos los caminos existentes con que tengan ciudad origen, ciudadOrigen, en caso de no existir devuelve null
-	public ArrayList<Camino> getCiudadOrigen (String ciudadOrigen){
+	public ArrayList<Camino> getCaminosConCiudadOrigen (String ciudadOrigen){
 		if (Caminos.containsKey(ciudadOrigen)) {
 			return Caminos.get(ciudadOrigen);
 		}
 		else return null; 
 	}
 
-    public void setCapacidad(String ciudadOrigen, String ciudadDestino, String transporte, int capacidad) throws Exception{
-		Camino auxCamino = new Camino(ciudadOrigen,ciudadDestino, transporte); //Camino auxiliar
-		if (Caminos.containsKey(ciudadOrigen)) { //primer miro si hi ha algun cami amb ciutatorigen
-			if (Caminos.get(ciudadOrigen).contains(auxCamino)){ //miro si existe ese camino en concreto
-				int pos = Caminos.get(ciudadOrigen).indexOf(auxCamino); //posicion donde esta el elemento
-				Caminos.get(ciudadOrigen).get(pos).setCapacidad(capacidad); //del mapa trec el vector, del array trec el cami, del cami consultu la capacitat
-			}
-			else {
-				throw new Exception("El Camino NO existe");   
-			}
-		}	
-		else { 
-			throw new Exception("El Camino NO existe");   
-		}
-	}
-		
-    
 	
 }
+
+
+
+
+
+
+
+/*
+public void setCapacidad(String ciudadOrigen, String ciudadDestino, String transporte, int capacidad) throws Exception{
+	Camino auxCamino = new Camino(ciudadOrigen,ciudadDestino, transporte); //Camino auxiliar
+	if (Caminos.containsKey(ciudadOrigen)) { //primer miro si hi ha algun cami amb ciutatorigen
+		if (Caminos.get(ciudadOrigen).contains(auxCamino)){ //miro si existe ese camino en concreto
+			int pos = Caminos.get(ciudadOrigen).indexOf(auxCamino); //posicion donde esta el elemento
+			Caminos.get(ciudadOrigen).get(pos).setCapacidad(capacidad); //del mapa trec el vector, del array trec el cami, del cami consultu la capacitat
+		}
+		else {
+			throw new Exception("El Camino NO existe");   
+		}
+	}	
+	else { 
+		throw new Exception("El Camino NO existe");   
+	}
+}
+	
+*/  
