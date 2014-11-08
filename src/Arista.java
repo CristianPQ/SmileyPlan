0b610e31312bca50406ca9c01c822b8ce0ea35d9
Daniel Villanueva <danielvillanueva94@gmail.com>
2014-11-08 20:23:17 +0100

    Arista

---
+++ src/Arista.java
@@ -0,0 +1,39 @@
+public class Arista{
+		private int verticeDestino;
+		private int flujo;
+		private int capacidad;
+		private int coste;
+
+		public int consultarVerticeDestino(){
+			return verticeDestino;
+		}
+
+		public void modificarVerticeDestino(int targetVertex){
+			verticeDestino = targetVertex;
+		}
+
+		public int consultarFlujo() {
+			return flujo;
+		}
+
+		public void modificarFlujo(int flow) {
+			flujo = flow;
+		}
+
+		public int consultarCapacidad() {
+			return capacidad;
+		}
+
+		public void modificarCapacidad(int capacity) {
+			capacidad = capacity;
+		}
+
+		public int consultarCoste() {
+			return coste;
+		}
+
+		public void modificarCoste(int cost) {
+			coste = cost;
+			}
+		
+}
--- src/Grafo.java
+++ src/Grafo.java
@@ -1,45 +1,6 @@
 import java.util.*;
 public class Grafo {
 
-	private class Arista{
-		private int verticeDestino;
-		private int flujo;
-		private int capacidad;
-		private int coste;
-
-		public int consultarVerticeDestino(){
-			return verticeDestino;
-		}
-
-		public void modificarVerticeDestino(int targetVertex){
-			verticeDestino = targetVertex;
-		}
-
-		public int consultarFlujo() {
-			return flujo;
-		}
-
-		public void modificarFlujo(int flow) {
-			flujo = flow;
-		}
-
-		public int consultarCapacidad() {
-			return capacidad;
-		}
-
-		public void modificarCapacidad(int capacity) {
-			capacidad = capacity;
-		}
-
-		public int consultarCoste() {
-			return coste;
-		}
-
-		public void modificarCoste(int cost) {
-			coste = cost;
-			}
-		
-	};
 	//constructora
 	ArrayList<Arista> grafo;
 
