25afd708c502f4d318f524228656975792b53c71
Daniel Villanueva <danielvillanueva94@gmail.com>
2014-11-08 19:25:57 +0100

    Grafo

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,80 +1,97 @@
 import java.util.*;
 public class Grafo {
 
-private class Arista{
-private int verticeDestino;
-private int flujo;
-private int capacidad;
-private int coste;
+	private class Arista{
+		private int verticeDestino;
+		private int flujo;
+		private int capacidad;
+		private int coste;
 
-public int consultarVerticeDestino(){
-	return verticeDestino;
-}
+		public int consultarVerticeDestino(){
+			return verticeDestino;
+		}
 
-public void modificarVerticeDestino(int targetVertex){
-	verticeDestino = targetVertex;
-}
+		public void modificarVerticeDestino(int targetVertex){
+			verticeDestino = targetVertex;
+		}
 
-public int consultarFlujo() {
-	return flujo;
-}
-public void modificarFlujo(int flow) {
-	flujo = flow;
-}
+		public int consultarFlujo() {
+			return flujo;
+		}
 
-public int consultarCapacidad() {
-	return capacidad;
-}
+		public void modificarFlujo(int flow) {
+			flujo = flow;
+		}
 
-public void modificarCapacidad(int capacity) {
-	capacidad = capacity;
-}
+		public int consultarCapacidad() {
+			return capacidad;
+		}
 
-public int consultarCoste() {
-	return coste;
-}
+		public void modificarCapacidad(int capacity) {
+			capacidad = capacity;
+		}
 
-public void ModificarCoste(int cost) {
-	coste = cost;
-}
+		public int consultarCoste() {
+			return coste;
+		}
 
-};
+		public void modificarCoste(int cost) {
+			coste = cost;
+			}
+		
+	};
 
-private class Grafo{
+	ArrayList<Arista> grafo = new ArrayList<Arista>();
+
+	public Grafo(){}
+
+	public int consultarVerticeDestinoArista
+		(int vertex){
+			Arista a = grafo.get(vertex);
+			return a.consultarVerticeDestino();
+	}
+
+	public void modificarVerticeDestinoArista
+		(int vertex, int nuevoFlujo) {
+			Arista a = grafo.get(vertex);
+			a.modificarVerticeDestino(nuevoFlujo);
+			grafo.add(vertex,a);
+	}
 	
-
-public int consultarFlujoArista(int vertex){
+	public int consultarFlujoArista(int vertex){
+	Arista a = grafo.get(vertex);
+	return a.consultarFlujo();
 	
+	}
+
+	public void modificarFlujoArista(int vertex, int nuevoFlujo) {
+		Arista a = grafo.get(vertex);
+		a.modificarFlujo(nuevoFlujo);
+		grafo.add(vertex,a);
+	}
+
+	public int consultarCapacidadArista(int vertex){
+		Arista a = grafo.get(vertex);
+		return a.consultarCapacidad();
 	
+	}
+
+	public void modificarCapacidadArista(int vertex, int nuevaCapacidad) {
+		Arista a = grafo.get(vertex);
+		a.modificarCapacidad(nuevaCapacidad);
+		grafo.add(vertex,a);
+	}
+
+	public int consultarCosteArista(int vertex){
+		Arista a = grafo.get(vertex);
+		return a.consultarCoste();
 	
-//	return flujo;
-}
+	}
 
-public void modificarFlujoArista(int flow) {
-//	flujo = flow;
-}
-
-public int consultarCapacidadArista() {
-//	return capacidad;
-}
-
-public void modificarCapacidadArista(int capacity) {
-//	capacidad = capacity;
-}
-
-public int consultarCosteArista() {
-	
-//	return coste;
-}
-
-public void ModificarCosteArista(int cost) {
-//	coste = cost;
-}
-
-
-};
-
-
-
+	public void modificarCosteArista(int vertex, int nuevoCoste) {
+		Arista a = grafo.get(vertex);
+		a.modificarCoste(nuevoCoste);
+		grafo.add(vertex,a);
+	}
 
 }
