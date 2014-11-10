12c50b638176d1f7f66fe5176ba5fc0677b4fa47
Cristian Pinto <quinto100@gmail.com>
2014-11-10 19:49:02 +0100

    FF en construcció

--- src/FordFulkerson.java
+++ src/FordFulkerson.java
@@ -1,8 +1,15 @@
+import java.util.*;
+
 
 public class FordFulkerson {
 	
-	public Grafo ejecutar(Grafo g, Grafo f, String[] it) {
-		
+	public Grafo ejecutar(Grafo g, Grafo f, String destino, ArrayList<String> rec) {
+		if(g != null) {
+			for(int i = g.hijos(); ++i) {
+				
+			}
+			
+		}
 		return g;
 	}
 }
