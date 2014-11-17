f19d193d66815def86255a44a3dfd56f8d2d03e0
Daniel Villanueva <danielvillanueva94@gmail.com>
2014-11-17 20:14:31 +0100

    -

--- src/DriverEntrada.java
+++ src/DriverEntrada.java
@@ -22,7 +22,7 @@ private static void menu() {
 }
 
 public static void main(String [] args) throws Exception {
-			GrafoAntiguo g = null;
+			Entrada e = null;
 	    	menu();
 	    	Scanner sc = new Scanner(System.in);
 	    	String[] lsplited;
@@ -33,7 +33,7 @@ public static void main(String [] args) throws Exception {
 	    			switch(Integer.parseInt(lsplited[0])){
 	    				case 1: {
 	    					int numVertex = Integer.parseInt(lsplited[1]);
-	    					g = new GrafoAntiguo(numVertex);
+	    					e = new Entrada(g, s, t, numAg);
 	    					break;
 	    				}
 	    				case 2:{//existe la arista?    				
@@ -55,84 +55,36 @@ public static void main(String [] args) throws Exception {
 	    				}
 	    				case 4:{ //eliminarArista
 	    					int vertex = Integer.parseInt(lsplited[1]);
-	    					int targetVertex = Integer.parseInt(lsplited[2]);
-	    					g.eliminarArista(vertex, targetVertex);
+
 	    					break;
 	    				}
 	    				
 	    				case 5: {//consultarFlujoArista
 	    					int vertex = Integer.parseInt(lsplited[1]);
-	    					int targetVertex = Integer.parseInt(lsplited[2]);
-	    					int flujoArista = g.consultarFlujoArista(vertex, targetVertex);
 	    					System.out.println("Flujo Arista ("+ vertex + "," +  targetVertex +"): "+ flujoArista +"\n");
 	    					break;
 	    				}
 	    				  				
 	    				case 6: {//modificarFlujoArista
 	    					int vertex = Integer.parseInt(lsplited[1]);
-	    					int targetVertex = Integer.parseInt(lsplited[2]);
-	    					int nuevoFlujo = Integer.parseInt(lsplited[3]);
-	    					g.modificarFlujoArista(vertex, targetVertex, nuevoFlujo);
 	    					break;
 	    				}
 	    				
 	    				case 7: {//consultarCapacidadArista
 	    					int vertex = Integer.parseInt(lsplited[1]);
-	    					int targetVertex = Integer.parseInt(lsplited[2]);
-	    					int capacidadArista = g.consultarCapacidadArista(vertex, targetVertex);
-	    					System.out.println("Capacidad Arista ("+ vertex + "," +  targetVertex +"): "+ capacidadArista +"\n");
 	    					break;
 	    				}
 	    				
 	    				case 8: {//modificarCapacidadArista
 	    					int vertex = Integer.parseInt(lsplited[1]);
-	    					int targetVertex = Integer.parseInt(lsplited[2]);
-	    					int nuevaCapacidad = Integer.parseInt(lsplited[3]);
-	    					g.modificarCapacidadArista(vertex, targetVertex, nuevaCapacidad);
 	    					break;
 	    				}
 	    				
 	    				case 9: {//ConsultarCosteArista
 	    					int vertex = Integer.parseInt(lsplited[1]);
-	    					int targetVertex = Integer.parseInt(lsplited[2]);
-	    					int costeArista = g.consultarCosteArista(vertex, targetVertex);
-	    					System.out.println("Coste Arista ("+ vertex + "," +  targetVertex +"): "+ costeArista +"\n");
 	    					break;
 	    				}
-	    				
-	    				case 10: {//modificarCosteArista
-	    					int vertex = Integer.parseInt(lsplited[1]);
-	    					int targetVertex = Integer.parseInt(lsplited[2]);
-	    					int nuevoCoste = Integer.parseInt(lsplited[3]);
-	    					g.modificarCapacidadArista(vertex, targetVertex, nuevoCoste);
-	    					break;
-	    				}
-	    				
-	    				case 11: {
-	    					int num = g.consultarNumVertices();
-	    					System.out.println("Numero de vertices:" + num + "\n");
-	    					break;
-	    					
-	    				}
-	    				
-	    				case 12: {
-	    					int vertex = Integer.parseInt(lsplited[1]);
-	    					int num = g.consultarNumAristasVertice(vertex);
-	    					System.out.println("Aristas desde vertice "+ vertex+ ": " + num + "\n");
-	    				
-	    					break;
-	    				}	
-	    				
-	    				case 13: {
-	    					int numVertex = Integer.parseInt(lsplited[1]);
-	    					ArrayList <Arista> l = g.consultarAdyacentes(numVertex);
-	    					for (int i = 0; i < l.size(); ++i){
-	    						System.out.println(l.get(i).consultarVerticeDestino()  + " ");
-	    					}
-	    					System.out.println("\n");
-	    					break;
-	    					
-	    				}	
+
 	    				
 	   		
 	    				case 0: {
