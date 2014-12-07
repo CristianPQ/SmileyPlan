488b54a08e92171b9db539137ef4486e97cb3a48
Daniel Villanueva <danielvillanueva94@gmail.com>
2014-12-07 11:40:03 +0100

    --

--- src/ControladorPresentacionAgentes.java
+++ src/ControladorPresentacionAgentes.java
@@ -2,5 +2,47 @@
 import javax.swing.*;
 
 public class ControladorPresentacionAgentes {
+	private static ControladorAgentes ca;
+	private static VistaAgentes vistaAg;
+	
+	public ControladorPresentacionAgentes(){
+		ca = new ControladorAgentes();
+		vistaAg = new VistaAgentes(this);
+		
+	}
+	
+	public void agregarAgente (String n, String ciuIni, String ciuObj, ControladorMapa m) 
+			throws Exception{
+				ca.anadirAgente(n, ciuIni, ciuObj, m);
+				}
+	
+	public void modificarNombre(String antiguo, String nuevo) throws Exception {
+		ca.modificarNombreAgente(antiguo, nuevo);
+	}
+	
+	public void modificarCiudadInicial(String n, String ciuIni) throws Exception{
+		ca.modificarCiudadInicialAgente(n, ciuIni);
+	}
+	
+	public void modificarCiudadObjetivo (String n, String ciuObj) throws Exception{
+		ca.modificarCiudadObjetivoAgente(n, ciuObj);
+	}
 
+	public void eliminarAgente(String n) throws Exception{
+		ca.eliminarAgente(n);
+	}
+	
+	public void guardarAgente(String path, String file) throws Exception {
+		ca.Guardar(file);
+	}
+	
+	public void cargarAgente(String path, String file) throws Exception {
+		ca.Cargar(file);
+	}
+
+	//*****************PANEL*****************************
+	
+	public static VistaAgentes getVista(){
+		return vistaAg;
+	}
 }
