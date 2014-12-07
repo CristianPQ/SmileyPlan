392a4873787e0873055b0721b5b4009122493422
Daniel Villanueva <danielvillanueva94@gmail.com>
2014-12-07 16:41:54 +0100

    -

--- src/ControladorPresentacionMapa.java
+++ src/ControladorPresentacionMapa.java
@@ -6,7 +6,28 @@ public class ControladorPresentacionMapa {
 	
 	
 	public ControladorPresentacionMapa(){
-		vm
+		vm = new VistaMapa(this);
+		
 		}
+	
+	public void crearMapa(int x, int y, String cont) throws Exception{
+		cm = new ControladorMapa(x, y, cont);
+	}
+	////////////////CIUDADES//////////////////
+	public void agregarCiudad(String n, int x, int y) throws Exception{
+		cm.agregarCiudad(n,x,y);
+
+	}
+	
+	public void eliminarCiudad(String c) throws Exception{
+		cm.eliminarCiudad(c);
+	}
+	
+	public void modificarCoordenadas(String n, int x, int y) throws Exception{
+		cm.modificarAtributosCiudad(n, x, y);
+	}
+	
+	
+	
 	}
 
