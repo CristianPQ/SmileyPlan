1abab4e8e7c551dcebf5ef38b08ca44af7537352
Daniel Villanueva <danielvillanueva94@gmail.com>
2014-12-07 12:37:48 +0100

    --

--- src/VistaAgentes.java
+++ src/VistaAgentes.java
@@ -1,4 +1,14 @@
-
+import java.awt.*;
+import javax.swing.*;
 public class VistaAgentes extends Vista1 {
-
+	
+	private ControladorPresentacionAgentes ctrlPAg;
+	
+	VistaAgentes(ControladorPresentacionAgentes cpag){
+		super();
+		ctrlPAg = cpag;
+		
+		
+	}
+	
 }
